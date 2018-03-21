8466fd6d8fbaea41aa68b164f7b84278760a8ef3
etronicspy <etronicspy@gmail.com>
2018-03-21 16:25:23 +0300

    Commit 3

--- src/App.js
+++ src/App.js
@@ -14,11 +14,7 @@ class App extends Component {
   render() {
 
     const {isOpen} = this.state;
-    /*const body = isOpen ? 
-    <div className="Button" onClick = {this.toggleOpen}>
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-    : null;*/
+    const {article} = this.props;
 
     return (
       <div className="App">
@@ -31,17 +27,21 @@ class App extends Component {
             {isOpen ? 'Stop': 'Start'}
           </button>
         </p>
-        <p className="App-intro">
+        <div className="App-intro">
           {this.getBody()}
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
+
+        
+
       </div>
     );
   }
 
   getBody() {
     if (!this.state.isOpen) return <p className="a">It seems to be ready!</p>
-    return <p className="b">It works!</p>
+    const {article} = this.props;
+    return <div> <p className="b">It works!</p> <p>{article.text}</p> </div>
   }
 
   toggleOpen = () => {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {articles} from './fixtures';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App article={articles[0]}/>, document.getElementById('root'));
 registerServiceWorker();
